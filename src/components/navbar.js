33887c0d0c1d52e2dd64811633f8c26d12d130f7
Paul Chesa <paulchesa1@gmail.com>
2021-04-07 16:44:17 +0300

    navbar bootstrap

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap'
 
-function Navbar() {
-    return (
-        <>
-       <Link to="/">Home </Link>
-       <Link to="/user">Profile </Link>
-       <Link to="/course">Courses </Link>
-        </>
-    )
+function Navibar() {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
+  return (
+    <>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Learning Portal</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse className="nav-items" isOpen={isOpen} navbar>
+          <div className="mover"></div>
+          <Nav className="mr-auto nav-links" navbar>
+            <NavItem>
+              <NavLink>
+                <Link to="/">Home</Link>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                <Link to="/course">Courses</Link>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                <Link to="/user">User</Link>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </>
+  )
 }
 
-export default Navbar
+export default Navibar
