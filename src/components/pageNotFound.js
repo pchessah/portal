665dadb408c01a05adcf3884e56e339fdf53a25a
Paul Chesa <paulchesa1@gmail.com>
2021-04-08 16:22:20 +0300

    Added error 404 gif

--- src/components/pageNotFound.js
+++ src/components/pageNotFound.js
@@ -1,11 +1,13 @@
 import React from 'react'
+// @ts-ignore
+import error404 from '../images/error404.gif'
 
 function PageNotFound() {
-    return (
-        <>
-        <h1>ERROR 404, PAGE NOT FOUND</h1>
-        </>
-    )
+  return (
+    <div className="error404-img-holder">
+      <img className="error404-img" src={error404} alt="error404" />
+    </div>
+  )
 }
 
 export default PageNotFound
