c3f9e72a60caada7fa499fada35b8b8bf897fa96
Paul Chesa <paulchesa1@gmail.com>
2021-04-11 11:25:18 +0300

    Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,24 @@
 import React from 'react'
-
+import { AiOutlineCopyright, AiFillTwitterCircle, AiFillYoutube } from 'react-icons/ai'
+import { FaFacebook } from 'react-icons/fa'
 function Footer() {
-    return (
-        <>
-        <div>
-            Footer
-        </div>
-        </>
-    )
+  return (
+    <>
+   
+      <div className="my-footer">
+      <p>Connect with us: </p> 
+        <span>
+          <FaFacebook />
+        </span>
+        <span>
+          <AiFillTwitterCircle />
+        </span>
+        <span>
+            <AiFillYoutube/>
+        </span>
+      </div>
+    </>
+  )
 }
 
 export default Footer
