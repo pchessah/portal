00de8fb1b92fed86a39a98cfa916bb8632a0ea18
Paul Chesa <paulchesa1@gmail.com>
2021-04-23 15:55:48 +0300

    youtube added

--- src/user/coursePage.js
+++ src/user/coursePage.js
@@ -3,7 +3,6 @@ import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
@@ -44,6 +43,8 @@ const useStyles = makeStyles((theme) => ({
     height: '100%',
     display: 'flex',
     flexDirection: 'column',
+    width: '350px',
+    margin: '5px',
   },
   cardMedia: {
     paddingTop: '56.25%', // 16:9
@@ -53,7 +54,11 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const course_cards = [
+  { playlist_id: 'https://www.youtube.com/embed?v=PkZNo7MFNFg' },
+  { playlist_id: 'https://www.youtube.com/embed?v=rfscVS0vtbw' },
+  { playlist_id: 'https://www.youtube.com/embed?v=eIrMbAQSU34' },
+]
 
 export default function CoursePage() {
   const classes = useStyles()
@@ -96,14 +101,14 @@ export default function CoursePage() {
         <Container className={classes.cardGrid} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={3}>
+            {course_cards.map((course_card) => (
+              <Grid item key={course_card.playlist_id} xs={12} sm={6} md={4}>
                 <Card className={classes.card}>
                   <CardContent className={classes.cardContent}>
                     <iframe
-                      width="853"
-                      height="480"
-                      src={`https://www.youtube.com/embed/${embedId}`}
+                      width="320"
+                      height="300"
+                      src={course_card.playlist_id}
                       frameBorder="0"
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                       allowFullScreen
